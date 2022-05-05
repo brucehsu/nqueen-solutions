28b42bf6e249d3845698bbfe04b5429f8db08e4f
Szu-Kai Hsu (brucehsu) <hi@brucehsu.org>
2022-05-05 14:22:13 +0100

    refactor: use hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Solutions from './components/Solutions';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {queenSize: 1};
-    this.sizeChange = this.sizeChange.bind(this);
-  }
-
-  sizeChange(event) {
-    this.setState({queenSize: event.target.value});
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <form>
-          <label>n: </label>
-          <select id="queen_size" value={this.state.queenSize} onChange={this.sizeChange}>
-            {(new Array(9)).fill(null).map((_, idx) => {
-              return (<option value={idx+1}  key={idx}>{idx+1}</option>)
-            })}
-          </select>
-        </form>
-        <Solutions queenSize={this.state.queenSize} />
-      </div>
-    );
-  }
+function App() {
+  const [queenSize, setQueenSize] = useState(1);
+  return (
+    <div className="App">
+      <form>
+        <label>n: </label>
+        <select id="queen_size" value={queenSize} onChange={(event) => setQueenSize(event.target.value)}>
+          {(new Array(9)).fill(null).map((_, idx) => {
+            return (<option value={idx+1}  key={idx}>{idx+1}</option>)
+          })}
+        </select>
+      </form>
+      <Solutions queenSize={queenSize} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Solutions.jsx
+++ src/components/Solutions.jsx
@@ -2,27 +2,19 @@ import React, {useMemo} from 'react';
 import {findNQueenSolution} from '../nqueen'
 import SolutionBlock from './SolutionBlock';
 
-class Solutions extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const {queenSize} = this.props
-    const solutions = findNQueenSolution(queenSize);
-    // const solutions = useMemo(() => findNQueenSolution(queenSize), [queenSize])
+function Solutions({queenSize}) {    
+    const solutions = useMemo(() => findNQueenSolution(queenSize), [queenSize]);
     return (
-      <div>
-          <div>
+        <div>
+            <div>
             Size of solutions: {solutions.length}
-          </div>
-          <hr />
-          {solutions.map((solution, idx) => {
-              return (<SolutionBlock solution={solution} key={"solution_" + idx}></SolutionBlock>)
-          })}
-      </div>
+            </div>
+            <hr />
+            {solutions.map((solution, idx) => {
+                return (<SolutionBlock solution={solution} key={"solution_" + idx}></SolutionBlock>)
+            })}
+        </div>
     );
-  }
 }
 
 export default Solutions;
